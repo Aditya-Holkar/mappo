bda930aeaeb054327f25df161376312965396c57
Aditya-Holkar <adiholkar555@gmail.com>
2024-11-05 22:45:53 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import dynamic from "next/dynamic";
 
-// Dynamically import the Map component with SSR disabled
 const Map = dynamic(() => import("./components/Map"), {
   ssr: false,
 });
